cea4033877ecc6649301daf4d2d536fa425a850e
Erno <ernohor@gmail.com>
2022-02-05 11:19:41 +0100

    cegesauto mod

--- cegesauto/cegesauto/Program.cs
+++ cegesauto/cegesauto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace cegesauto
 {
@@ -8,8 +9,8 @@ namespace cegesauto
         struct Autok
         {
             public int nap { get; set; }
-            public int ora { get; set; }
-            public int perc { get; set; }
+            public string ora { get; set; }
+            public string perc { get; set; }
             public string rendszam { get; set; }
             public int szemelyi { get; set; }
             public int km { get; set; }
@@ -24,8 +25,8 @@ namespace cegesauto
             for (int i = 0; i < autok.Length; i++)
             {
                 autok[i].nap = Convert.ToInt32(file[i].Split(" ")[0]);
-                autok[i].ora = Convert.ToInt32(file[i].Split(" ")[1].Substring(0, 2));
-                autok[i].perc = Convert.ToInt32(file[i].Split(" ")[1].Substring(3, 2));
+                autok[i].ora =file[i].Split(" ")[1].Substring(0, 2);
+                autok[i].perc = file[i].Split(" ")[1].Substring(3, 2);
                 autok[i].rendszam = file[i].Split(" ")[2];
                 autok[i].szemelyi = Convert.ToInt32(file[i].Split(" ")[3]);
                 autok[i].km = Convert.ToInt32(file[i].Split(" ")[4]);
@@ -73,6 +74,7 @@ namespace cegesauto
                 }
             }
 
+            Console.WriteLine("4. feladat");
             Console.WriteLine($"A hónap végén {(autok.Length - (2 * osszeg))}  autót nem hoztak vissza. ");
 
             List<string> rendszamok = new List<string>();
@@ -110,8 +112,77 @@ namespace cegesauto
                     }
                 }
 
+                Console.WriteLine("5. feladat");
                 Console.WriteLine($"{rendszamok[i]} {max-min}");
             }
+
+            Console.WriteLine("6. feladat");
+
+            List<int> szemelyik = new List<int>();
+
+            for (int i = 0; i < autok.Length; i++)
+            {
+                if (!szemelyik.Contains(autok[i].szemelyi))
+                {
+                    szemelyik.Add(autok[i].szemelyi);
+                }
+            }
+
+            int ossz = 0;
+            int ki = 0;
+            int be = 0;
+            int szemely = 0;
+
+            for (int i = 0; i < szemelyik.Count; i++)
+            {
+                for (int j = 0; j < autok.Length; j++)
+                {
+                    if (szemelyik[i] == autok[j].szemelyi)
+                    {
+                        if (autok[j].kibe == 0)
+                        {
+                            ki = autok[j].km;
+                        }
+
+                        if (autok[j].kibe == 1)
+                        {
+                            be = autok[j].km;
+
+                            if ((be - ki) > ossz)
+                            {
+                                ossz = be - ki;
+                                szemely = szemelyik[i];
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Leghosszabb út: {ossz} km, személy: {szemely}");
+
+            Console.WriteLine("7. feladat");
+
+            Console.Write("Rendszám: ");
+            string rendszam = Console.ReadLine();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+                for (int i = 0; i < autok.Length; i++)
+                {
+                    if (autok[i].rendszam == rendszam)
+                    {
+                        if (autok[i].kibe == 0)
+                        {
+                            stringBuilder.Append($"{autok[i].szemelyi}\t{autok[i].nap}. {autok[i].ora}:{autok[i].perc}\t{autok[i].km} km \t");
+                       
+                        } else
+                        {
+                            stringBuilder.Append($"{autok[i].nap}. {autok[i].ora}:{autok[i].perc}\t{autok[i].km} km\n");
+                        }
+                    }
+                }
+
+            Console.WriteLine(stringBuilder);
         }
     }
 }
